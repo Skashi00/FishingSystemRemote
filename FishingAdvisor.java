bd978a79ff55fa6591f2e492aab12813eb8d09db
sebastianbackstrom <j.sebastian.backstrom@gmail.com>
2025-05-14 21:07:10 +0200

    Välkommen statement + Scanner objekt implementerat

--- FishingAdvisor.java
+++ FishingAdvisor.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class FishingAdvisor {
     public static void main(String[] args){
+        // User input //
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n=== Välkommen till FishingAdvisor ===");
+        System.out.println("Din guide till bättre fiske, varje gång.\n");
+
+
+
+        scanner.close();
 
     }
 }
